3967e9e14765b8820224e24c5383e399334fc2b1
Martin Lutzemann <mlutzemann@outlook.com>
2021-12-05 14:28:04 +0100

    add day 01 task 2 solution

--- day-01/solution.py
+++ day-01/solution.py
@@ -1,8 +1,34 @@
-def count_increases(text_input):
+# helper function
+def convert_to_int_list(str_list):
+    return list(map(int, str_list))
+
+
+# solution to task 1
+def count_increases(num_input):
     last_value = None
     counter = 0
-    for num in text_input:
-        counter += 0 if last_value is None else (0 if int(num) <= last_value else 1)
-        last_value = int(num)
+    for num in num_input:
+        counter += 0 if last_value is None else (0 if num <= last_value else 1)
+        last_value = num
 
     return counter
+
+
+# solution to task 2
+def sliding_window(num_input):
+    size = 3
+    window_sums = []
+    for i in range(len(num_input) - size + 1):
+        window_sums.append(num_input[i] + num_input[i + 1] + num_input[i + 2])
+
+    return window_sums
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        test_data = convert_to_int_list(f.readlines())
+        sliding_window_list = sliding_window(test_data)
+
+        sliding_window_increases = count_increases(sliding_window_list)
+
+        print(sliding_window_increases)
--- day-01/test.py
+++ day-01/test.py
@@ -1,16 +1,16 @@
 import unittest
 
-from solution import count_increases
+from solution import convert_to_int_list, count_increases, sliding_window
 
 
 class Day01Test(unittest.TestCase):
-    def test_sample_input(self):
+    def test_sample_increases(self):
         """
         Test if the sample input yields the right results.
         """
         with open("sample.txt", "r") as f:
             # arrange
-            test_data = f.readlines()
+            test_data = convert_to_int_list(f.readlines())
 
             # act
             count = count_increases(test_data)
@@ -18,19 +18,56 @@ class Day01Test(unittest.TestCase):
             # assert
             self.assertEqual(count, 7)
 
-    def test_main_input(self):
+    def test_main_increases(self):
         """
         Test if the main input yields the right results.
         """
         with open("input.txt", "r") as f:
             # arrange
-            test_data = f.readlines()
+            test_data = convert_to_int_list(f.readlines())
 
             # act
             count = count_increases(test_data)
 
             # assert
             self.assertEqual(count, 1393)
+
+    def test_sliding_window_sample(self):
+        with open("sample.txt", "r") as f:
+            # arrange
+            test_data = convert_to_int_list(f.readlines())
+
+            # act
+            sliding_window_list = sliding_window(test_data)
+
+            # assert
+            self.assertEqual(
+                sliding_window_list, [607, 618, 618, 617, 647, 716, 769, 792]
+            )
+
+    def test_sliding_window_sample_increases(self):
+        with open("sample.txt", "r") as f:
+            # arrange
+            test_data = convert_to_int_list(f.readlines())
+            sliding_window_list = sliding_window(test_data)
+
+            # act
+            sliding_window_increases = count_increases(sliding_window_list)
+
+            # assert
+            self.assertEqual(sliding_window_increases, 5)
+
+    def test_sliding_window_input_increases(self):
+        with open("input.txt", "r") as f:
+            # arrange
+            test_data = convert_to_int_list(f.readlines())
+            sliding_window_list = sliding_window(test_data)
+
+            # act
+            sliding_window_increases = count_increases(sliding_window_list)
+
+            # assert
+            self.assertEqual(sliding_window_increases, 1359)
 
 
 if __name__ == "__main__":
