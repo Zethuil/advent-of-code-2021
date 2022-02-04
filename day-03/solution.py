d4e80669395a575f0162477bc3b653c0a410839d
Martin Lutzemann <martin.lutzemann@valtech.com>
2022-02-04 17:02:19 +0100

    add solution for day 03

--- day-03/solution.py
+++ day-03/solution.py
@@ -1,2 +1,83 @@
+from sqlalchemy import false
+from sqlalchemy.sql.expression import true
+
+
+def find_most_frequent_value(input_list, bit_index):
+    counter = 0
+    for number in input_list:
+        counter += 1 if number[bit_index] == "1" else -1
+
+    return "1" if counter >= 0 else "0"
+
+
+def filter_by_bit(input_list, bit_index, value):
+    result_list = []
+    for number in input_list:
+        if number[bit_index] == value:
+            result_list.append(number)
+
+    return result_list
+
+
+def invert_bits(input_binary):
+    output_binary = ""
+    output_binary = output_binary.join(
+        ["1" if input_binary[i] == "0" else "0" for i in range(len(input_binary))]
+    )
+
+    return output_binary
+
+
+def calculate_advanced_rating(input_list, lower_frequency=False):
+    new_list = input_list
+    for i in range(len(input_list[0])):
+        bit_value = find_most_frequent_value(new_list, i)
+        new_list = filter_by_bit(
+            new_list, i, invert_bits(bit_value) if lower_frequency else bit_value
+        )
+
+        if len(new_list) == 1:
+            break
+
+    return new_list[0]
+
+
+def calculate_oxygen_generator_rating(input_list):
+
+    return calculate_advanced_rating(input_list)
+
+
+def calculate_co2_scrubber_rating(input_list):
+
+    return calculate_advanced_rating(input_list, lower_frequency=True)
+
+
+def calculate_life_support_rating(input_list):
+    oxygen = calculate_oxygen_generator_rating(input_list)
+    scrubber = calculate_co2_scrubber_rating(input_list)
+
+    return int(oxygen, 2) * int(scrubber, 2)
+
+
+def calculate_gamma_rate(input_list):
+    gamma = ""
+    for i in range(len(input_list[0])):
+        gamma += find_most_frequent_value(input_list, i)
+
+    return gamma
+
+
+def calculate_epsilon_rate(gamma_rate):
+
+    return invert_bits(gamma_rate)
+
+
+def calculate_power_consumption(input_list):
+    gamma = calculate_gamma_rate(input_list)
+    epsilon = calculate_epsilon_rate(gamma)
+
+    return int(gamma, 2) * int(epsilon, 2)
+
+
 if __name__ == "__main__":
     print("...running")
--- day-03/test.py
+++ day-03/test.py
@@ -1,10 +1,139 @@
 import unittest
 
-import solution
+from solution import (
+    calculate_gamma_rate,
+    calculate_epsilon_rate,
+    calculate_life_support_rating,
+    calculate_power_consumption,
+    filter_by_bit,
+    calculate_oxygen_generator_rating,
+    calculate_co2_scrubber_rating,
+)
 
 
-class DayXYTest(unittest.TestCase):
-    pass
+class Day03Test(unittest.TestCase):
+    def test_simple_input_gamma_rate(self):
+        # arrange
+        input_list = ["00100", "11110", "10110"]
+
+        # act
+        gamma_rate = calculate_gamma_rate(input_list)
+
+        # assert
+        self.assertEqual(gamma_rate, "10110")
+
+    def test_simple_epsilon_rate(self):
+        # arrange
+        input_list = ["00100", "11110", "10110"]
+        gamma_rate = calculate_gamma_rate(input_list)
+
+        # act
+        epsilon_rate = calculate_epsilon_rate(gamma_rate)
+
+        # assert
+        self.assertEqual(epsilon_rate, "01001")
+
+    def test_sample_input_gamma_rate(self):
+        # arrange
+        with open("sample.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+
+        # act
+        gamma_rate = calculate_gamma_rate(input_list)
+
+        # assert
+        self.assertEqual(gamma_rate, "10110")
+
+    def test_sample_input_epsilon_rate(self):
+        # arrange
+        with open("sample.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+            gamma_rate = calculate_gamma_rate(input_list)
+
+        # act
+        epsilon_rate = calculate_epsilon_rate(gamma_rate)
+
+        # assert
+        self.assertEqual(epsilon_rate, "01001")
+
+    def test_sample_input_power_consumption(self):
+        # arrange
+        with open("sample.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+
+        # act
+        power_consumption = calculate_power_consumption(input_list)
+
+        # assert
+        self.assertEqual(power_consumption, 198)
+
+    def test_main_input_power_consumption(self):
+        # arrange
+        with open("input.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+
+        # act
+        power_consumption = calculate_power_consumption(input_list)
+
+        # assert
+        self.assertEqual(power_consumption, 693486)
+
+    def test_sample_input_filter_by_bit(self):
+        # arrange
+        with open("sample.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+
+        # act
+        actual_list = filter_by_bit(input_list, 0, "1")
+
+        # assert
+        self.assertEqual(
+            actual_list, ["11110", "10110", "10111", "10101", "11100", "10000", "11001"]
+        )
+
+    def test_sample_input_oxygen_rating(self):
+        # arrange
+        with open("sample.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+
+        # act
+        actual = calculate_oxygen_generator_rating(input_list)
+
+        # assert
+        self.assertEqual(actual, "10111")
+
+    def test_sample_input_scrubber_rating(self):
+        # arrange
+        with open("sample.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+
+        # act
+        actual = calculate_co2_scrubber_rating(input_list)
+
+        # assert
+        self.assertEqual(actual, "01010")
+
+    def test_sample_input_life_support_rating(self):
+        # arrange
+        with open("sample.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+
+        # act
+        actual = calculate_life_support_rating(input_list)
+
+        # assert
+        self.assertEqual(actual, 230)
+
+    def test_main_input_life_support_rating(self):
+        # arrange
+        with open("input.txt", "r") as input_file:
+            input_list = input_file.read().splitlines()
+
+        # act
+        actual = calculate_life_support_rating(input_list)
+
+        # assert
+        self.assertEqual(actual, 3379326)
 
 
 if __name__ == "__main__":
